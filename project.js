98e5790a6e101eb2799141c3cee6714a92308b7d
AliceDiCarlo <37734553+AliceDiCarlo@users.noreply.github.com>
2021-06-12 12:53:20 +0200

    screen settings

--- project.js
+++ project.js
@@ -1,1 +1,26 @@
- 
+
+var delayTime = 10;
+/*
+var delayTime = 1000,
+    updateTime = 500;
+*/
+
+
+/* ****************
+ settings display and screens
+ **************** */
+
+//margini
+var margin = {top: 10, right: 60, bottom: 40, left: 20};
+// var margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+
+// screen 800x 300 o 1600x900
+//define the margins
+var width = 1600 - margin.left - margin.right;
+var height = 900 - margin.top - margin.bottom;
+
+/* 
+var width = 800 - margin.left - margin.right;
+var height = 300 - margin.top - margin.bottom;
+*/
